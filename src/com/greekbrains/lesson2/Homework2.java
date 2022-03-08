e1dd9623ac1febcf28dfe0d8bcca6087ef8e8bdc
evgeny104 <nazawka88@gmail.com>
2022-03-08 18:05:04 +0300

    completing the first task

--- src/com/greekbrains/lesson2/Homework2.java
+++ src/com/greekbrains/lesson2/Homework2.java
@@ -1,4 +1,18 @@
 package com.greekbrains.lesson2;
 
 public class Homework2 {
+
+    public static void main(String[] args) {
+        boolean a = sumAB(1, 20);
+            System.out.println("Результат " + a);
+    }
+    private static boolean sumAB(int a, int b) {
+        int result = a + b;
+        if ( result >= 10 && result <= 20) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
 }
