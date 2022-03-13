a8d83fd4cc7b6fb84d4b3c39b914b517adc10a0c
evgeny104 <nazawka88@gmail.com>
2022-03-13 16:46:29 +0300

    created a branch lesson2

--- src/com/greekbrains/lesson2/Homework2.java
+++ src/com/greekbrains/lesson2/Homework2.java
@@ -3,12 +3,12 @@ package com.greekbrains.lesson2;
 public class Homework2 {
 
     public static void main(String[] args) {
-        boolean a = sumAB(1, 20);
+        boolean a = sumAB(-10, 20);
             System.out.println("Результат " + a);
     }
     private static boolean sumAB(int a, int b) {
         int result = a + b;
-        if ( result >= 10 && result <= 20) {
+        if (result >= 10 && result <= 20) {
             return false;
         } else {
             return true;
