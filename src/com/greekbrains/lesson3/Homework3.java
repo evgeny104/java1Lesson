a0071f3ccb8d3f59d65aec729935f464886c9501
evgeny104 <nazawka88@gmail.com>
2023-03-28 15:29:22 +0300

    test

--- src/com/greekbrains/lesson3/Homework3.java
+++ src/com/greekbrains/lesson3/Homework3.java
@@ -10,42 +10,47 @@ public class Homework3 {
         int[][] array = new int[4][4];
         int lastIndex = array.length - 1;
         fillDiagonal(array, lastIndex);
-        initArray(1,2);
-        System.out.println(Arrays.toString(initArray(5,6)));
+        initArray(1, 2);
+        System.out.println(Arrays.toString(initArray(5, 6)));
     }
-    public static void numbers () {
+
+    public static void numbers() {
         int[] arrData = {1, 1, 0, 0, 1, 1, 0, 0,};
         for (int i = 0; i < arrData.length; i++) {
-            if ( arrData[i] == 1 ) {
+            if (arrData[i] == 1) {
                 arrData[i] = 0;
-            } else if ( arrData[i] == 0)
+            } else if (arrData[i] == 0)
                 arrData[i] = 1;
         }
         System.out.println(Arrays.toString(arrData));
     }
-    public static void longArray () {
+
+    public static void longArray() {
         int[] arrData = new int[100];
         for (int i = 0; i < arrData.length; i++) {
-            arrData[i] = i + 1 ;
+            arrData[i] = i + 1;
         }
         System.out.println(Arrays.toString(arrData));
     }
+
     public static void multiTwo() {
         int[] arrData = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arrData.length; i++) {
             int a = 6;
-            if (arrData[i] <= a){
+            if (arrData[i] <= a) {
                 arrData[i] *= 2;
             }
         }
         System.out.println(Arrays.toString(arrData));
     }
+
     private static void fillDiagonal(int[][] array, int lastIndex) {
         for (int i = 0; i < array.length; i++) {
             array[i][i] = array[i][lastIndex - i] = 1;
             System.out.println(Arrays.toString(array[i]));
         }
     }
+
     private static int[] initArray(int len, int initialValue) {
         int[] array = new int[len];
         for (int i = 0; i < array.length; i++) {
